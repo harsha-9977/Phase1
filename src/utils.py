7b6dc65f6fe19e2edc1c46853d489ab3ef0f87f0
harsha-9977 <prakashas404@gmail.com>
2025-04-29 13:48:16 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -3,4 +3,4 @@
 import pandas as pd
 
 def load_crime_data():
-    return pd.read_csv(r'C:\Users\Prajwal\OneDrive\Documents\GitHub\Phase1\data\crime.csv', encoding='latin1')
+    return pd.read_csv(r'data\crime.csv', encoding='latin1')
